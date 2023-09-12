572b4a01fe593b535fba7451ef131b9de4df09c9
Zach Riane Machacon <zachriane01@gmail.com>
2023-09-12 21:43:06 +0800

    fix: enabled user input

--- add_two_nums.asm
+++ add_two_nums.asm
@@ -1,14 +1,30 @@
-org 100h
+org 100h   
     
     MOV DI, 0                   ; Loads offset for display to 0
     MOV DX, 0                   ; Loads sum container - DX to  0
     LEA SI, MSG1                ; Loads memory address of MSG1 to source index
     CALL SHOW_MSG
+    
+    MOV AH, 0
+    CALL 0xF400:0x01C0
+    MOV BL, AL
+    MOV ES:[DI], BL   
+    ADD DI, 2h  
+    
     CALL ADD_NUM
     ADD DI, 80H                 ; DI is moved by an offset of 80 to go to next line
       
     LEA SI, MSG2
     CALL SHOW_MSG 
+    
+    PUSH SP                            
+    MOV AH, 0
+    CALL 0xF400:0x01C0
+    MOV BL, AL
+    MOV ES:[DI], BL   
+    ADD DI, 2h 
+    POP SP 
+    
     CALL ADD_NUM 
     ADD DI, 80H
        
@@ -19,28 +35,27 @@ org 100h
     MOV ES:[DI], DL             ; Prints on current offset
 ret
 
-MSG1 DB 'INPUT 1ST VAL: 2', 0        
-MSG2 DB 'INPUT 2ND VAL: 7', 0 
-MSG3 DB 'SUM IS: ', 0
+MSG1 DB 'INPUT 1ST VAL: ', 0        
+MSG2 DB 'INPUT 2ND VAL: ', 0 
+MSG3 DB 'SUM IS: ', 0 
 
 SHOW_MSG:
     PUSH AX                     ; Stores AX to the stack 
     PUSH SI                     ; Stores SI to the stack
     MOV AX, 0B800H              ; Stores to AX the address of video display
-    MOV ES, AX                  ; Stores to ES the address of video display                
-    MOV AX, CS  
-    MOV DS, AX                  ; Stores CS to DS
-        SHOW:
-            CMP [SI], 0         ; Compares current character to 0
-            JZ EXIT             ; If 0, exit 
-            MOV BL, CS:[SI]     ; Store current ASCII to BL for summing 
-            MOVSB               ; Copy byte at DS:[SI] to ES:[DI]. Increment SI and DI.
-            INC DI              ; DI is incremented one more time since each character is 2 bytes
-        LOOP SHOW 
+    MOV ES, AX                  ; Stores to ES the address of video display  
+    MOV AX, CS
+    MOV DS, AX              
+    SHOW:
+        CMP [SI], 0             ; Compares current character to 0
+        JZ EXIT                 ; If 0, exit 
+        MOVSB                   ; Copy byte at DS:[SI] to ES:[DI]. Increment SI and DI.
+        INC DI                  ; DI is incremented one more time since each character is 2 bytes
+    LOOP SHOW 
     EXIT:   
         POP SI                  ; Restores values of SI and AX
-        POP AX      
-    RET
+        POP AX     
+    RET     
 
 ADD_NUM:
     SUB BL, 30h                 ; Gets actual integer value from BL by subtracting 30 hex
--- print_template.asm
+++ print_template.asm
@@ -6,7 +6,13 @@ org 100h
     
     mov DI, 0           ; Initializes offset of display to 0. This makes it appear on the top-left of the screen.
     lea SI, GREETING    ; Loads GREETING's memory address to SI. SI is used since GREETING will be the source of our data. Hence the name, SOURCE INDEX.
-    call DISPLAY_MSG    
+    call DISPLAY_MSG 
+    lea SI, GREETING
+    call INVERT_MSG 
+    
+    ADD DI, 100H
+    sub SI, DX
+    call DISPLAY_MSG     
 
 ret
 
@@ -16,6 +22,8 @@ DISPLAY_MSG:
     pusha                   ; Pushes all register values to the stack. This is just good practice but not required. We usually only push values we want their values saved.
     mov AX, 0B800h          ; Moves the address of the CGA display to AX. It has to go through AX since we cannot directly move it to ES. Only register -> segment MOVs are allowed.
     mov ES, AX              ; Moves the address from AX to ES. This is explained by the last comment.
+    mov AX, CS              
+    mov DS, AX
     SHOW:
          cmp [SI], 0        ; Current character is compared to 0. This is because our null terminator is the 0.
          jz EXIT            ; If previous cmp returns TRUE or 1, then call EXIT
@@ -27,4 +35,34 @@ DISPLAY_MSG:
         ret
 
 INVERT_MSG:
-    
+    mov AX, CS
+    mov DS, AX
+    mov CX, 0
+
+    ; Count the number of characters in the string
+    COUNT_LOOP:
+        cmp [SI], 0
+        jz REVERSE_LOOP
+        inc CX
+        inc SI
+        mov DX, CX
+        jmp COUNT_LOOP 
+
+    REVERSE_LOOP:
+        cmp CX, 0
+        jz EXIT_INVERT
+        dec SI                 ; Point SI to the previous character
+        push DS:[SI]           ; Push the character onto the stack
+        dec CX
+        jnz REVERSE_LOOP
+
+    POP_LOOP: 
+        cmp [SI], 0
+        jz EXIT_INVERT
+        pop AX
+        mov [SI], AL           ; Store the character back to its original position
+        inc SI
+    loop POP_LOOP
+
+    EXIT_INVERT:
+        ret 
